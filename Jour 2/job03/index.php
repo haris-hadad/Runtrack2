1eca0ba629c1180cd758b06056d67f48dfa60acb
haris-hadad <haris.hadad@laplateforme.io>
2025-09-25 14:10:52 +0200

    affichage nombre de 0 a 100 avec des modifications

--- Jour 2/job03/index.php
+++ Jour 2/job03/index.php
@@ -2,16 +2,19 @@
 for ($i = 0; $i <= 100; $i++) {
     if ($i >= 0 && $i <= 20) {
 
-        echo "<i>$i</i><br />"; }
-    elseif ($i >= 25 && $i <= 50) {
-        if($i === 42){
-              echo "LaPlateforme<br />";
-        }
-            echo "<u>$i</u><br />";
+        echo "<i>$i</i><br />";
+        
+    } elseif ($i >= 25 && $i <= 50) {
+        if ($i == 42) {
 
-     }
-    
-    else{
-        echo $i . "<br />";
+            echo "<u>La Plateforme_</u><br />";
+        } else {
+
+            echo "<u>$i</u><br />";
+        }
+    } else { {
+            echo "$i<br />";
+        }
     }
 }
+?>
